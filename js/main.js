b700ec8eae5826381d76aab1a35976d74b2f3c65
Md Ashik <mostofabilla@gmail.com>
2019-12-23 03:17:17 +0600

    gallery design done.

--- js/main.js
+++ js/main.js
@@ -1,6 +1,22 @@
-$(document).ready(function () {
+// Category on click feature.
 
-// main js Start
-  // type your code 
-// main js end
+$(document).ready(function () {
+    const selector = '#galleryCategoryList ul li';
+    $(selector).on('click', function () {
+        $(selector).removeClass('active');
+        $(this).addClass('active');
+    })
 });
+
+
+
+// Category on click feature.
+
+// $(document).ready(function () {
+//     const selector = '#galleryCategoryList ul li';
+//     $(selector).on('click', function () {
+//         $(selector).addClass('active');
+//         $(this).addClass('active');
+//     })
+// });
+
